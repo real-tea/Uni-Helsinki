8fb23802f888fe0621951834910038e7bfd39681
Akash <akash.0711a@gmail.com>
2021-09-07 12:12:31 +0530

    new components and completing the unicafe assignment

---
+++ src/components/statstics.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Statistics = ( { text,value } ) => {
+    return (
+        <>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+            
+        </>
+    )
+}
+
+export default Statistics
